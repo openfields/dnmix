436cf81ae331201ac01aa1b95b9345c5f7e59622
Fields <1538480017@mil>
2017-08-24 15:32:51 -0500

    modified title

--- R/bpv.r
+++ R/bpv.r
@@ -9,7 +9,8 @@ bpv <- function(mcmc, title){
   f.lim <- c(min(mcmc$sims.list$fit.new), max(mcmc$sims.list$fit.new), min(mcmc$sims.list$fit), max(mcmc$sims.list$fit))
   p.lim <- c(min(f.lim), max(f.lim))
   # create plot & add 1:1 line
-  plot(mcmc$sims.list$fit.new,mcmc$sims.list$fit,xlim=p.lim,ylim=p.lim, main=title)
+  tex <- paste(title, "\n", "Bayesian p-value:", " ", round(bpv,4))
+  plot(mcmc$sims.list$fit.new,mcmc$sims.list$fit,xlim=p.lim,ylim=p.lim, main=tex)
   abline(0,1,col="red")
   # return Bayesian p-value
   return(bpv)
