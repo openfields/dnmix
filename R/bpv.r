8bd0fb3041762a2a4073b2a3b14e1840f35ff1d3
Fields <1538480017@mil>
2017-08-24 15:20:25 -0500

    added parameter to allow for figure title

--- R/bpv.r
+++ R/bpv.r
@@ -2,14 +2,14 @@
 # requires 'fit.new' and 'fit' objects within sims.list object from model fit with jagsUI
 # created by WRF, 24 Aug 2017
 
-bpv <- function(mcmc){
+bpv <- function(mcmc, title){
   # calculate Bayesian p-value
   bpv <- mean(mcmc$sims.list$fit.new>mcmc$sims.list$fit)
   # determine min/max for plot
   f.lim <- c(min(mcmc$sims.list$fit.new), max(mcmc$sims.list$fit.new), min(mcmc$sims.list$fit), max(mcmc$sims.list$fit))
   p.lim <- c(min(f.lim), max(f.lim))
   # create plot & add 1:1 line
-  plot(mcmc$sims.list$fit.new,mcmc$sims.list$fit,xlim=p.lim,ylim=p.lim)
+  plot(mcmc$sims.list$fit.new,mcmc$sims.list$fit,xlim=p.lim,ylim=p.lim, main=title)
   abline(0,1,col="red")
   # return Bayesian p-value
   return(bpv)
