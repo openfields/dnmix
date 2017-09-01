15323e69bcf2fec6a06faeef0d78e7591a3628d3
Fields <1538480017@mil>
2017-09-01 13:34:32 -0500

    changed indentation

--- R/pois_sel.r
+++ R/pois_sel.r
@@ -29,28 +29,28 @@ cat("
       pi0[i]<- 1 - mu[i,1]-mu[i,2]-mu[i,3]
       pcap[i]<-1-pi0[i]
     
-    for(j in 1:3){
-    muc[i,j]<-mu[i,j]/pcap[i]
-    }
+      for(j in 1:3){
+        muc[i,j]<-mu[i,j]/pcap[i]
+      }
     
-    # 1. model part 1: the conditional multinomial
-    y[i,1:3] ~ dmulti(muc[i,1:3],ncap[i])
+      # 1. model part 1: the conditional multinomial
+      y[i,1:3] ~ dmulti(muc[i,1:3],ncap[i])
     
-    # 2. model for the observed count of uniques
-    ncap[i] ~ dbin(pcap[i],N[i])
+      # 2. model for the observed count of uniques
+      ncap[i] ~ dbin(pcap[i],N[i])
     
-    # 3. abundance model
-    N[i] ~ dpois(lambda[i])
-    log(lambda[i])<- beta0 + w1*beta1*cov1[i] + w2*beta2*cov2[i] + w3*beta3*cov3[i] + w4*beta4*cov4[i] + w5*beta5*cov5[i]+ w6*beta6*cov6[i] + w7*beta7*cov7[i]
+      # 3. abundance model
+      N[i] ~ dpois(lambda[i])
+      log(lambda[i])<- beta0 + w1*beta1*cov1[i] + w2*beta2*cov2[i] + w3*beta3*cov3[i] + w4*beta4*cov4[i] + w5*beta5*cov5[i]+ w6*beta6*cov6[i] + w7*beta7*cov7[i]
     
-    # fit stats
-    for (j in 1:3){
-    eval[i,j] <- p[i]*N[i]
-    E[i,j] <- pow((y[i,j] - eval[i,j]),2)/(eval[i,j]+0.5)
+      # fit stats
+      for (j in 1:3){
+        eval[i,j] <- p[i]*N[i]
+        E[i,j] <- pow((y[i,j] - eval[i,j]),2)/(eval[i,j]+0.5)
     
-    y.new[i,j] ~ dbin(p[i], N[i])
-    E.new[i,j] <- pow((y.new[i,j] - eval[i,j]),2)/(eval[i,j]+0.5)
-    }
+        y.new[i,j] ~ dbin(p[i], N[i])
+        E.new[i,j] <- pow((y.new[i,j] - eval[i,j]),2)/(eval[i,j]+0.5)
+      }
     
     }
     
@@ -61,3 +61,4 @@ cat("
     }
     ",fill=TRUE)
 sink()
+
